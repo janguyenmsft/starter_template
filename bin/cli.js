0fa5d8f0ac5bf76ba102c7b0ddfc78ddb0190026
Jasmine Nguyen <janguyen@microsoft.com>
2023-02-03 12:33:10 +0000

    Update bin file

---
+++ bin/cli.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const { execSync } = require('child-process');
+
+const runCommand = (command) => {
+    try {
+        execSync(`${command}`, { stdio: 'inherit' });
+    } catch (e) {
+        console.error(`Failed to execute ${command}`, e);
+        return false;
+    }
+
+    return true;
+}
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/janguyenmsft/starter_template ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) {
+    process.exit(-1);
+}
+
+console.log(`Installing dependencies for ${repoName}`);
+const installDeps = runCommand(installDepsCommand);
+if (!installDeps) {
+    process.exit(-1);
+}
+
+console.log(`Congratulations! You are ready, run the following command to start`);
+console.log(`cd ${repoName} && npm start`)
